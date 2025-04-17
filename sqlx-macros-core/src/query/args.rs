68b10a7fd9466587a496df51e117098db9054b55
reachingforthejack <lazdotdigital@gmail.com>
2025-04-16 21:27:25 -0700

    remove override types, add automatic from conversions

--- sqlx-macros-core/src/query/args.rs
+++ sqlx-macros-core/src/query/args.rs
@@ -5,7 +5,6 @@ use proc_macro2::TokenStream;
 use quote::{format_ident, quote, quote_spanned};
 use sqlx_core::describe::Describe;
 use syn::spanned::Spanned;
-use syn::{Expr, ExprCast, ExprGroup, Type};
 
 /// Returns a tokenstream which typechecks the arguments passed to the macro
 /// and binds them to `DB::Arguments` with the ident `query_args`.
@@ -26,77 +25,62 @@ pub fn quote_args<DB: DatabaseExt>(
         .collect::<Vec<_>>();
 
     let arg_name = &arg_names;
-    let arg_expr = input.arg_exprs.iter().cloned().map(strip_wildcard);
 
-    let arg_bindings = quote! {
-        #(let #arg_name = &(#arg_expr);)*
+    let Some(Either::Left(params)) = info.parameters() else {
+        unimplemented!("only normal parameter inputs are supported safely");
     };
 
-    let args_check = match info.parameters() {
-        None | Some(Either::Right(_)) => {
-            // all we can do is check arity which we did
-            TokenStream::new()
-        }
+    let params = params
+        .iter()
+        .map(|param| {
+            DB::param_type_for_id(param)
+                .expect("only basic types are supported")
+                .parse::<TokenStream>()
+                .expect("unparsable type, consider migrating your db to a simpler type")
+        })
+        .collect::<Vec<_>>();
 
-        Some(Either::Left(_)) if !input.checked => {
-            // this is an `*_unchecked!()` macro invocation
-            TokenStream::new()
-        }
+    let arg_bindings = input
+        .arg_exprs
+        .iter()
+        .cloned()
+        .zip(params.iter())
+        .zip(arg_name)
+        .map(|((expr, arg_name), param)| -> TokenStream {
+            quote_spanned!(expr.span() => {
+                // TODO: make something like `sqlx::DbFrom` so that these from impls can be
+                // disambiguated from any other from impl
+                let #arg_name = &(<#param as ::core::convert::From>::from(#expr));
+            })
+        })
+        .collect::<TokenStream>();
 
-        Some(Either::Left(params)) => {
-            params
-                .iter()
-                .zip(arg_names.iter().zip(&input.arg_exprs))
-                .enumerate()
-                .map(|(i, (param_ty, (name, expr)))| -> crate::Result<_> {
-                    if get_type_override(expr).is_some() {
-                        // cast will fail to compile if the type does not match
-                        // and we strip casts to wildcard
-                        return Ok(quote!());
-                    }
+    let args_check = params
+        .iter()
+        .zip(arg_names.iter().zip(&input.arg_exprs))
+        .map(|(param_ty, (name, expr))| -> crate::Result<_> {
+            Ok(quote_spanned!(expr.span() =>
+                // this shouldn't actually run
+                #[allow(clippy::missing_panics_doc, clippy::unreachable)]
+                if false {
+                    use ::sqlx::ty_match::{WrapSameExt as _, MatchBorrowExt as _};
 
-                    let param_ty =
-                        DB::param_type_for_id(param_ty)
-                            .ok_or_else(|| {
-                                if let Some(feature_gate) = DB::get_feature_gate(param_ty) {
-                                    format!(
-                                        "optional sqlx feature `{}` required for type {} of param #{}",
-                                        feature_gate,
-                                        param_ty,
-                                        i + 1,
-                                    )
-                                } else {
-                                    format!("unsupported type {} for param #{}", param_ty, i + 1)
-                                }
-                            })?
-                            .parse::<TokenStream>()
-                            .map_err(|_| format!("Rust type mapping for {param_ty} not parsable"))?;
+                    // evaluate the expression only once in case it contains moves
+                    let expr = ::sqlx::ty_match::dupe_value(#name);
 
-                    Ok(quote_spanned!(expr.span() =>
-                        // this shouldn't actually run
-                        #[allow(clippy::missing_panics_doc, clippy::unreachable)]
-                        if false {
-                            use ::sqlx::ty_match::{WrapSameExt as _, MatchBorrowExt as _};
+                    // if `expr` is `Option<T>`, get `Option<$ty>`, otherwise `$ty`
+                    let ty_check = ::sqlx::ty_match::WrapSame::<#param_ty, _>::new(&expr).wrap_same();
 
-                            // evaluate the expression only once in case it contains moves
-                            let expr = ::sqlx::ty_match::dupe_value(#name);
+                    // if `expr` is `&str`, convert `String` to `&str`
+                    let (mut _ty_check, match_borrow) = ::sqlx::ty_match::MatchBorrow::new(ty_check, &expr);
 
-                            // if `expr` is `Option<T>`, get `Option<$ty>`, otherwise `$ty`
-                            let ty_check = ::sqlx::ty_match::WrapSame::<#param_ty, _>::new(&expr).wrap_same();
+                    _ty_check = match_borrow.match_borrow();
 
-                            // if `expr` is `&str`, convert `String` to `&str`
-                            let (mut _ty_check, match_borrow) = ::sqlx::ty_match::MatchBorrow::new(ty_check, &expr);
-
-                            _ty_check = match_borrow.match_borrow();
-
-                            // this causes move-analysis to effectively ignore this block
-                            ::std::unreachable!();
-                        }
-                    ))
-                })
-                .collect::<crate::Result<TokenStream>>()?
-        }
-    };
+                    // this causes move-analysis to effectively ignore this block
+                    ::std::unreachable!();
+                }
+        ))})
+        .collect::<crate::Result<TokenStream>>()?;
 
     let args_count = input.arg_exprs.len();
 
@@ -113,43 +97,4 @@ pub fn quote_args<DB: DatabaseExt>(
         let query_args = ::core::result::Result::<_, ::sqlx::error::BoxDynError>::Ok(query_args)
         #(.and_then(move |mut query_args| query_args.add(#arg_name).map(move |()| query_args) ))*;
     })
-}
-
-fn get_type_override(expr: &Expr) -> Option<&Type> {
-    match expr {
-        Expr::Group(group) => get_type_override(&group.expr),
-        Expr::Cast(cast) => Some(&cast.ty),
-        _ => None,
-    }
-}
-
-fn strip_wildcard(expr: Expr) -> Expr {
-    match expr {
-        Expr::Group(ExprGroup {
-            attrs,
-            group_token,
-            expr,
-        }) => Expr::Group(ExprGroup {
-            attrs,
-            group_token,
-            expr: Box::new(strip_wildcard(*expr)),
-        }),
-        // we want to retain casts if they semantically matter
-        Expr::Cast(ExprCast {
-            attrs,
-            expr,
-            as_token,
-            ty,
-        }) => match *ty {
-            // cast to wildcard `_` will produce weird errors; we interpret it as taking the value as-is
-            Type::Infer(_) => *expr,
-            _ => Expr::Cast(ExprCast {
-                attrs,
-                expr,
-                as_token,
-                ty,
-            }),
-        },
-        _ => expr,
-    }
 }
